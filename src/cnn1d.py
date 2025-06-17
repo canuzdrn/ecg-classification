f42a885d93803451816f9ab9a8bedc801dc4da81
Can <uzdurancan@gmail.com>
2025-06-17 22:38:53 +0200

    development of model no.2 which is a 1d cnn

---
+++ src/cnn1d.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+class CNN1DModel(nn.Module):
+    """
+    1d cnn model for univariate ecg signal classification
+    """
+    def __init__(self, n_classes=4):
+        super().__init__()
+
+
+        # remember it is 1d cnn
+        # overall arch 1 -> 16 -> 32 -> 4 (logits)
+        self.net = nn.Sequential(
+            nn.Conv1d(1, 16, kernel_size=7, stride=1, padding=3),
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+
+            nn.Conv1d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1)  # (batch size, 64, 1)
+        )
+        self.classifier = nn.Linear(64, n_classes)
+
+    def forward(self, x_list, lengths=None):
+        """
+        x_list: list of 1d tensors with VARYING lengths
+        Returns -- logits of shape (batch, num_classes = 4)
+        """
+        # pad all to max length -- remember that lengths vary throughout the dset
+        max_len = max([x.size(0) for x in x_list])
+        padded = torch.zeros(len(x_list), 1, max_len, device=x_list[0].device)
+
+        for i, x in enumerate(x_list):
+            padded[i, 0, :x.size(0)] = x  # zero padding at the end (TODO : maybe divide padding into begining and end?)
+
+        x = self.net(padded)  # (batch, 64, 1)
+        x = x.squeeze(-1)     # (batch, 64)
+
+        return self.classifier(x)  # (batch, # of classes = 4)
+    
+
+
+
+
+
+    
